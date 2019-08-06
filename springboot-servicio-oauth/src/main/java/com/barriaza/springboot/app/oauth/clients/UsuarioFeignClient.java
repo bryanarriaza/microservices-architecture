a601619ff0c85927b09e2fb0438105b7328bb25b
Bryan Arriaza <bjarriaza.g.07@gmail.com>
2019-08-05 22:41:12 -0600

    Setting spring security oauth2

---
+++ springboot-servicio-oauth/src/main/java/com/barriaza/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,14 @@
+package com.barriaza.springboot.app.oauth.clients;
+
+import com.barriaza.springboot.app.usuarios.commons.models.entity.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "servicio-usuarios")
+public interface UsuarioFeignClient {
+
+    @GetMapping("/usuarios/search/buscar-username")
+    public Usuario findByUsername(@RequestParam String username);
+
+}
